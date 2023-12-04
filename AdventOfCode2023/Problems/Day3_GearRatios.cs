fea594d7e3ec0a0ea1485537932a31e8c01d9c19
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-12-04 19:11:57 +0500

    Updated solution for Day3, solved the second puzzle

--- AdventOfCode2023/Problems/Day3_GearRatios.cs
+++ AdventOfCode2023/Problems/Day3_GearRatios.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode2023.Problems
     public class Day3_GearRatios
     {
         private readonly string _inputPath;
-
+        
         private readonly (int X, int Y)[] _directions =
         {
             (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)
@@ -19,6 +19,8 @@ namespace AdventOfCode2023.Problems
             '+', '-', '*', '/', '@', '#', '$', '%', '&', '='
         };
 
+        private readonly Dictionary<(int X, int Y), List<int>> _gears = new Dictionary<(int X, int Y), List<int>>();
+
         public Day3_GearRatios(string inputPath)
         {
             _inputPath = inputPath;
@@ -28,6 +30,7 @@ namespace AdventOfCode2023.Problems
         {
             var lines = FileOperations.ReadLines(_inputPath);
             var result = 0;
+            var resultGearRatio = 0;
 
             for (var i = 0; i < lines.Count; i++)
             {
@@ -39,38 +42,71 @@ namespace AdventOfCode2023.Problems
                 }
             }
 
+            foreach (var gear in _gears)
+            {
+                Console.WriteLine($"{lines[gear.Key.X][gear.Key.Y]} {string.Join(" ", gear.Value)}");
+                
+                if (gear.Value.Count != 2)
+                {
+                    continue;
+                }
+
+                var product = 1;
+                foreach (var number in gear.Value)
+                {
+                    product *= number;
+                }
+
+                resultGearRatio += product;
+            }
+
             Console.WriteLine($"Sum: {result}");
+            Console.WriteLine($"Gear Ratio: {resultGearRatio}");
         }
 
         private int ProcessMatch(Match match, List<string> lines, int row)
         {
             var isSymbolAdjacent = false;
+            var matchValue = int.Parse(match.Value);
 
             for (var i = match.Index; i < match.Index + match.Length; i++)
             {
-                var matchX = row;
-                var matchY = i;
-
                 foreach (var direction in _directions)
                 {
-                    var x = matchX + direction.X;
-                    var y = matchY + direction.Y;
+                    var x = row + direction.X;
+                    var y = i + direction.Y;
 
                     if (x < 0 || x >= lines.Count || y < 0 || y >= lines[0].Length)
                     {
                         continue;
                     }
 
-                    if (_matchableSymbols.Any(c => lines[x][y] == c))
+                    if (_matchableSymbols.All(c => lines[x][y] != c) || isSymbolAdjacent)
                     {
-                        isSymbolAdjacent = true;
+                        continue;
+                    }
+
+                    isSymbolAdjacent = true;
+
+                    if (lines[x][y] != '*')
+                    {
+                        continue;
+                    }
+
+                    if (!_gears.TryGetValue((x, y), out var numbers))
+                    {
+                        _gears[(x, y)] = new List<int> { matchValue };
+                    }
+                    else
+                    {
+                        numbers.Add(matchValue);
                     }
                 }
             }
 
-            Console.WriteLine($"isSymbolAdjacent: {match.Value} {isSymbolAdjacent}");
+            Console.WriteLine($"{match.Value}, isSymbolAdjacent: {isSymbolAdjacent}");
 
-            return !isSymbolAdjacent ? 0 : int.Parse(match.Value);
+            return !isSymbolAdjacent ? 0 : matchValue;
         }
     }
 }
