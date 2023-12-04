b3bb535ae927a95cbc1e8c5c9ca28b4d3804e996
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-12-04 23:25:57 +0500

    Updated solution for Day4, solved the second puzzle

--- AdventOfCode2023/Problems/Day4_Scratchcards.cs
+++ AdventOfCode2023/Problems/Day4_Scratchcards.cs
@@ -18,8 +18,10 @@ namespace AdventOfCode2023.Problems
         {
             var lines = FileOperations.ReadLines(_inputPath);
             var result = CalculateSum(lines);
+            var totalCardCount = CalculateTotalCardCount(lines);
 
             Console.WriteLine($"Sum: {result}");
+            Console.WriteLine($"Total Card Count: {totalCardCount}");
         }
 
         private int CalculateSum(List<string> lines)
@@ -34,17 +36,58 @@ namespace AdventOfCode2023.Problems
             return sum;
         }
 
+        private int CalculateTotalCardCount(List<string> lines)
+        {
+            var totalCardCount = 0;
+            var cardCounts = new int[lines.Count];
+
+            for (var i = 0; i < cardCounts.Length; i++)
+            {
+                cardCounts[i] = 1;
+            }
+
+            for (var i = 0; i < lines.Count; i++)
+            {
+                var price = ProcessLine(lines[i]);
+                var winningCardsCount = CalculateWinningCardsCount(price);
+
+                for (var j = 1; j <= winningCardsCount; j++)
+                {
+                    if (i + j >= lines.Count)
+                    {
+                        break;
+                    }
+
+                    cardCounts[i + j] += cardCounts[i];
+                }
+
+                totalCardCount += cardCounts[i];
+            }
+
+            return totalCardCount;
+        }
+
         private int ProcessLine(string line)
         {
             var cardInfo = line.Split(':');
             var cardDescription = cardInfo.Last();
             var cardNumber = cardDescription.Split('|');
+
             var winningNumbersString = cardNumber.First().Trim();
             var winningNumbers = ProcessNumbers(winningNumbersString);
+
             var userNumbersString = cardNumber.Last().Trim();
             var userNumbers = ProcessNumbers(userNumbersString);
+
+            var price = CalculateCardPrice(winningNumbers, userNumbers);
+
+            return price;
+        }
+
+        private int CalculateCardPrice(List<int> winningNumbers, List<int> userNumbers)
+        {
             var price = 0;
-            
+
             foreach (var userNumber in userNumbers)
             {
                 if (!winningNumbers.Contains(userNumber))
@@ -65,6 +108,16 @@ namespace AdventOfCode2023.Problems
             return price;
         }
 
+        private int CalculateWinningCardsCount(int price)
+        {
+            if (price == 0)
+            {
+                return 0;
+            }
+
+            return (int)(Math.Log(price, 2) + 1);
+        }
+
         private List<int> ProcessNumbers(string numbersString)
         {
             var numbers = new List<int>();
@@ -82,7 +135,7 @@ namespace AdventOfCode2023.Problems
                     numberStringBuilder.Clear();
                 }
             }
-            
+
             numbers.Add(int.Parse(numberStringBuilder.ToString()));
             return numbers;
         }
