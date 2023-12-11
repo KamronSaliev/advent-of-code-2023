919fc87eee7e1262d6ac5013249440cc9eef93d4
Komroniddin Soliev <kamron.saliev5@gmail.com>
2023-12-11 18:14:13 +0500

    Added Day 8 second puzzle solution

--- AdventOfCode2023/Problems/Day8/Day8_HauntedWasteland.cs
+++ AdventOfCode2023/Problems/Day8/Day8_HauntedWasteland.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using AdventOfCode2023.Common;
 
@@ -29,9 +30,34 @@ namespace AdventOfCode2023.Problems.Day8
             
             var result1 = CountSteps(instructions, map);
             Console.WriteLine($"Steps: {result1}");
+            
+            var result2 = CountStepsToEndingZNodes(instructions, map);
+            Console.WriteLine($"Steps to nodes Z: {result2}");
+        }
+
+        private char[] ProcessInstructions(int lineIndex, List<string> lines)
+        {
+            return lines[lineIndex].ToCharArray();
+        }
+
+        private Dictionary<string, (string, string)> ProcessMap(ref int lineIndex, List<string> lines)
+        {
+            var map = new Dictionary<string, (string, string)>();
+
+            while (lineIndex < lines.Count && lines[lineIndex] != string.Empty)
+            {
+                var line = lines[lineIndex];
+                var parsedData = Regex.Matches(line, "\\w+");
+                map.Add(parsedData[0].Value, (parsedData[1].Value, parsedData[2].Value));
+                lineIndex++;
+            }
+
+            lineIndex += MapLineIndexIncrement;
+
+            return map;
         }
         
-        private int CountSteps(char[] instructions, Dictionary<string, (string L, string R)> map)
+        private long CountSteps(char[] instructions, Dictionary<string, (string L, string R)> map)
         {
             var currentKey = "AAA";
             var count = 0;
@@ -57,27 +83,57 @@ namespace AdventOfCode2023.Problems.Day8
 
             return count;
         }
-
-        private char[] ProcessInstructions(int lineIndex, List<string> lines)
+        
+        private long CountStepsToEndingZNodes(char[] instructions, Dictionary<string, (string L, string R)> map)
         {
-            return lines[lineIndex].ToCharArray();
+            var endingANodes = map.Keys.Where(key => key.EndsWith("A")).ToList();
+            var steps = new List<long>();
+
+            foreach (var node in endingANodes)
+            {
+                var currentKey = node;
+                var count = 0;
+
+                while (!currentKey.EndsWith("Z"))
+                {
+                    var instruction = instructions[count % instructions.Length];
+
+                    switch (instruction)
+                    {
+                        case 'L':
+                            currentKey = map[currentKey].L;
+                            count++;
+                            break;
+                        case 'R':
+                            currentKey = map[currentKey].R;
+                            count++;
+                            break;
+                        default:
+                            throw new InvalidOperationException("Invalid instruction");
+                    }
+                }
+                
+                steps.Add(count);
+
+            }
+            
+            return CalculateLCM(steps);
         }
 
-        private Dictionary<string, (string, string)> ProcessMap(ref int lineIndex, List<string> lines)
+        private long CalculateLCM(IEnumerable<long> numbers)
         {
-            var map = new Dictionary<string, (string, string)>();
+            return numbers.Aggregate((long)1, (current, number) => current / CalculateGCD(current, number) * number);
+        }
 
-            while (lineIndex < lines.Count && lines[lineIndex] != string.Empty)
+        private long CalculateGCD(long a, long b)
+        {
+            while (b != 0)
             {
-                var line = lines[lineIndex];
-                var parsedData = Regex.Matches(line, "\\w+");
-                map.Add(parsedData[0].Value, (parsedData[1].Value, parsedData[2].Value));
-                lineIndex++;
+                a %= b;
+                (a, b) = (b, a);
             }
-
-            lineIndex += MapLineIndexIncrement;
-
-            return map;
+            
+            return a;
         }
     }
 }
